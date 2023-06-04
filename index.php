4cb6fabeb09ab3a8fc98e955d6e68590201470c3
Daniel Manosalva <manosalvasierra@gmail.com>
2023-06-04 15:59:58 -0500

    feat: :recycle: nueva calculadora moderna
    el codifo fue cambiado por completo, un diseño moderno y un codigo php mas funcional y leible
    
    dannkol

--- index.php
+++ index.php
@@ -5,177 +5,167 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>calculadora</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
+    <?php
+    function calcular()
+    {
+        $string = implode($_SESSION['numeros']);
+
+        $inputString = $string;
+
+        // Formar un array de números
+        $numbers = preg_split('/\+|\-|\×|\÷/', $inputString);
+
+        // Formar un array de operadores
+        $operators = preg_split('/[0-9]|\./', $inputString, -1, PREG_SPLIT_NO_EMPTY);
+
+        // Realizar las divisiones
+        $divide = array_search('÷', $operators);
+        while ($divide !== false) {
+            $numbers[$divide] = $numbers[$divide + 1] == '0' ? 0: $numbers[$divide] / $numbers[$divide + 1];
+            array_splice($numbers, $divide + 1, 1);
+            array_splice($operators, $divide, 1);
+            $divide = array_search('÷', $operators);
+        }
+
+        // Realizar las multiplicaciones
+        $multiply = array_search('×', $operators);
+        while ($multiply !== false) {
+            $numbers[$multiply] = $numbers[$multiply] * $numbers[$multiply + 1];
+            array_splice($numbers, $multiply + 1, 1);
+            array_splice($operators, $multiply, 1);
+            $multiply = array_search('×', $operators);
+        }
 
 
+
+
+        // Realizar las sumas y restas
+        $result = $numbers[0];
+        for ($i = 1; $i < count($numbers); $i++) {
+            $operator = $operators[$i - 1];
+            $operand = (float)$numbers[$i];
+            if ($operator === '+') {
+                $result += $operand;
+            } else if ($operator === '-') {
+                $result -= $operand;
+            }
+        }
+
+        return $result;
+    }
+    ?>
     <?php
 
-    session_start();
 
+    $resultado = (float) 0;
 
-    function res($valor)
-    {
-        session_reset();
-        $_SESSION = null;
-        return $valor = null;
+    if (!isset($_SESSION['numeros'])) {
+        $_SESSION['numeros'] = [];
     }
-
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        session_start();
+
+
 
         if (isset($_POST['numero'])) {
-
-            $valor = $_POST['numero'];
-            if (!isset($_SESSION['valores'])) {
-                $_SESSION['valores'] = (array) [];
-            }
-            $_SESSION['valores'][] = $valor;
-        } else if (isset($_POST['operacion'])) {
-            $valor = $_POST['operacion'];
-            if (!isset($_SESSION['valores'])) {
-                $_SESSION['valores'] = (array) [];
-            }
-            $valor = match ($valor) {
-                "resultado" => res($valor),
-            };
-            $_SESSION['valores'][] = $valor;
-        }
-    }
-
-    // Imprimir los valores del array
-    if (isset($_SESSION['valores'])) {
-        $valores = $_SESSION['valores'];
-        /* echo implode($valores); */
-        $datos = [];
-        $numero = '';
-
-        foreach ($_SESSION['valores'] as $data) {
-            if (is_numeric($data)) {
-                $numero .= $data;
+            $numero = $_POST['numero'];
+            if ($numero == 'clear') {
+                $_SESSION['numeros'] = [];
+            } else if ($numero == 'resultado') {
+                $resultado = calcular();
+            } else if($numero == 'delate'){
+                array_pop($_SESSION['numeros']);
             } else {
-                if (!empty($numero)) {
-                    $datos[] = (int) $numero;
-                    $numero = '';
-                }
+                array_push($_SESSION['numeros'], $numero);
             }
         }
-
-        if (!empty($numero)) {
-            $datos[] = (int) $numero;
-        }
-
-        $string = implode($_SESSION['valores']);
-
-
-        $string = str_replace(' ', '', $string);
-        $string = strrev($string);
-
-
-        $operadores = ['*', '/', '+', '-'];
-
-
-        $resultado = (float)$string;
-        $i = 1;
-
-        echo strrev($string)." =\n";
-
-        while ($i < strlen($string)) {
-            $operador = $string[$i];
-            $numero = '';
-
-            while ($i < strlen($string) && !in_array($string[$i + 1], $operadores)) {
-                $numero .= $string[$i + 1];
-                $i++;
-            }
-            switch ($operador) {
-                case '*':
-                    $resultado *= (float)$numero;
-                    break;
-                case '/':
-                    if ((float)$numero != 0) {
-                        $resultado /= (float)$numero;
-                    } else {
-                        $resultado = NAN;
-                    }
-                    break;
-                case '+':
-                    $resultado += (float)$numero;
-                    break;
-                case '-':
-                    $resultado -= (float)$numero;
-                    break;
-            }
-
-            $i++;
-        }
-
-        
     }
 
-
     ?>
-
     <form method="POST">
-        <button type="submit" name="numero" value="1">1</button>
-        <button type="submit" name="numero" value="2">2</button>
-        <button type="submit" name="numero" value="3">3</button><br>
-        <button type="submit" name="numero" value="4">4</button>
-        <button type="submit" name="numero" value="5">5</button>
-        <button type="submit" name="numero" value="6">6</button><br>
-        <button type="submit" name="numero" value="7">7</button>
-        <button type="submit" name="numero" value="8">8</button>
-        <button type="submit" name="numero" value="9">9</button><br>
-        <button type="submit" name="operacion" value="resultado">x</button>
-        <button type="submit" name="numero" value="+">+</button>
-        <button type="submit" name="numero" value="-">-</button>
-        <button type="submit" name="numero" value="*">*</button>
-        <button type="submit" name="numero" value="/">/</button>
-    </form>
-    <?php
-        echo $resultado;
+        <div class="container">
+            <div class="calc-body">
+                <div class="calc-screen">
+                    <div class="calc-operation"><?php echo implode($_SESSION['numeros'])?> </div>
+                    <div class="calc-typed"><?php print_r(round($resultado , 3 , PHP_ROUND_HALF_UP)); ?><span class="blink-me">_</span></div>
+                </div>
+                <div class="calc-button-row">
+                    <div class="none"></div>
+                    <button type="submit" name="numero" value="clear">
+                        <div class="button c">C</div>
+                    </button>
+                    <button type="submit" name="numero" value="delate">
+                        <div class="button c" style="color:orange;">D</div>
+                    </button>
+                    <button type="submit" name="numero" value="÷">
+                        <div class="button l">÷</div>
+                    </button>
+                </div>
+                <div class="calc-button-row">
+                    <button type="submit" name="numero" value="7">
+                        <div class="button">7</div>
+                    </button>
+                    <button type="submit" name="numero" value="8">
+                        <div class="button">8</div>
+                    </button>
+                    <button type="submit" name="numero" value="9">
+                        <div class="button">9</div>
+                    </button>
+                    <button type="submit" name="numero" value="×">
+                        <div class="button l">×</div>
+                    </button>
+                </div>
+                <div class="calc-button-row">
+                    <button type="submit" name="numero" value="4">
+                        <div class="button">4</div>
+                    </button>
+                    <button type="submit" name="numero" value="5">
+                        <div class="button">5</div>
+                    </button>
+                    <button type="submit" name="numero" value="6">
+                        <div class="button">6</div>
+                    </button>
+                    <button type="submit" name="numero" value="-">
+                        <div class="button l">-</div>
+                    </button>
+                </div>
+                <div class="calc-button-row">
+                    <button type="submit" name="numero" value="1">
+                        <div class="button">1</div>
+                    </button>
 
-    ?>
+                    <button type="submit" name="numero" value="2">
+                        <div class="button">2</div>
+                    </button>
+                    <button type="submit" name="numero" value="3">
+                        <div class="button">3</div>
+                    </button>
+                    <button type="submit" name="numero" value="+">
+                        <div class="button l">+</div>
+                    </button>
+                </div>
+                <div class="calc-button-row">
+                    <button type="submit" name="numero" value=".">
+                        <div class="button">.</div>
+                    </button>
+                    <button type="submit" name="numero" value="0">
+                        <div class="button">0</div>
+                    </button>
+
+                    <div class="none"></div>
+
+                    <button type="submit" name="numero" value="resultado">
+                        <div class="button l">=</div>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
 </body>
 
 </html>
-
-<!-- Posible solucion -->
-
-
-<!-- <?php
-    
-    $cad = '22-15+2+2/2*5';
-    $op = ['+','-','*','/'];
-    
-    
-    
-    $pattern = '/[' . preg_quote('+-*/', '/') . ']/';
-    $splt=  preg_split($pattern, $cad, -1, PREG_SPLIT_NO_EMPTY);
-    $op = preg_split('/[0-9]+/', $cad, -1, PREG_SPLIT_NO_EMPTY);
-    $result = preg_split($pattern, $cad);
-    
-    $res_op = (float) $result[0];
-    
-    for($i = 0; $i<count($op); $i++ ){
-
-        $res_op = match($op[$i]){
-            '+' => $res_op += $result[$i + 1],
-            '-' => $res_op -= $result[$i + 1],
-            '*' => $res_op *= $result[$i + 1],
-            '/' => $res_op /= $result[$i + 1],
-            default => null,
-        };
-    };
-    
-    print_r($res_op);
- 
-   
-    
-?> -->
-
-
-
-
-
